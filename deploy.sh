79950ebc430fc4e924bc0f236c7a3f528d8bb0b2
sethlee <1532084282@qq.com>
2021-02-09 17:55:44 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,18 @@ npm run build
 cd blog/.vuepress/dist
 
 # 如果是发布到自定义域名
-echo 'sethlee.site' > CNAME
+# echo 'sethlee.site' >CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
+# git pull origin master
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:sethlee23/sethlee23.github.io.git master
+git push -f git@github.com:SethLee23/SethLee23.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:SethLee23/vuepress-blog.git master:gh-pages
 # git push -f git@github.com:sethlee23/vuepress-blog.git master:gh-pages
 
 cd -
